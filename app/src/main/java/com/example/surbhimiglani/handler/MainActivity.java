13fa7e4a29cc4b5a6c7402b3d9ed0c8b92acedcf
Surbhi Miglani <me.surbhimiglani@gmail.com>
2017-10-06 02:49:57 +0530

    first

---
+++ app/src/main/java/com/example/surbhimiglani/handler/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.surbhimiglani.handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+   Handler
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
